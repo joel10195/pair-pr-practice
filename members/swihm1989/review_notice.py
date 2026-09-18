704ef32f875305de8b6d7b645b149a55f73cee3f
David <swihm1989@gmail.com>
2026-09-18 17:36:54 +0900

    branch 바꿈

--- members/swihm1989/review_notice.py
+++ members/swihm1989/review_notice.py
@@ -1,6 +1,10 @@
+def print_member_info(member, age, year):
+	print(f"{member}: 나이 {age} 구력 {year}년")
+
+
 members = ["민수", "철수", "영희", "민철"]
 ages = [30, 32, 31, 29]
 years = [1, 3, 5, 4]
 
 for member, age, year in zip(members, ages, years):
-	print(f"{member}: 나이 {age} 구력 {year}년")
+	print_member_info(member, age, year)
